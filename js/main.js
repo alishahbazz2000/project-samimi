78ba7ef9285b421380ddd43331ac1cee181b8f58
Ali Shahbaz <96702773+RedMaster2022@users.noreply.github.com>
2023-01-18 17:45:30 +0330

    first

---
+++ js/main.js
@@ -0,0 +1,8 @@
+ityped.init(document.querySelector("#ityped"), {
+  showCursor: false,
+  strings: [
+    "پروژه HSL تحليل و طراحي سيستم ها",
+    "خوش آمدید!",
+    "در این جا میتوانید اسامی کسایی که توی این پروژه شرکت داشتند رو مشاهده کنید"
+  ]
+});
